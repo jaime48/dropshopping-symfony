ca66f610b81d2c9f019f3793562728e0c8136c96
yang <yang@team.cruisewatch.com>
2020-08-11 09:20:14 +0200

    categories

--- dropshopping/vendor/composer/autoload_real.php
+++ dropshopping/vendor/composer/autoload_real.php
@@ -13,6 +13,9 @@ class ComposerAutoloaderInit5ca5ed4ac3d0163ef0ee344b502d7469
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
